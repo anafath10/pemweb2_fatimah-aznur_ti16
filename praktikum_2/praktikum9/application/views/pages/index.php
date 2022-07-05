5189675395c17c0f03cab3ae7d84945a05d8be36
anafath10 <92621896+anafath10@users.noreply.github.com>
2022-07-05 17:45:02 +0700

    fatimah

---
+++ praktikum_2/praktikum9/application/views/pages/index.php
@@ -0,0 +1,11 @@
+<div class="container mt-4">
+    <div class="row ">
+        <div class="col">
+            <h2 class="display-6">Praktikum9 - Login + CRUD</h2>
+            <hr />
+            <p class="text-center m-0">NIM: 0110221183</p>
+            <p class="text-center m-0">Nama: Fatimah Aznur</p>
+            <p class="text-center m-0">Rombel: TI16</p>
+        </div>
+    </div>
+</div>
